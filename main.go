932bc6cdf97ff66a25c41f1cc118c17b78444ace
Jaromir Humbitious <jaromir.vangorsel@gmail.com>
2017-11-06 14:35:13 +0100

    Add Duplicate check

--- main.go
+++ main.go
@@ -21,49 +21,52 @@ type chanDirInfo struct {
 }
 
 var extesionMap = map[string]string{
-	".JPG":     ".JPG",
-	".JPEG":    ".JPEG",
-	".PNG":     ".PNG",
-	".GIF":     ".GIF",
-	".RAW":     ".RAW",
-	".RW2":     ".RW2",
-	".TIF":     ".TIF",
-	".AI":      ".AI",
-	".MOV":     ".MOV",
-	".RAR":     ".RAR",
-	".MPG":     ".MPG",
-	".AFPHOTO": ".AFPHOTO",
-	".AVI":     ".AVI",
-	".M4V":     ".M4V",
-	".WMV":     ".WMV",
-	".MP4":     ".MP4",
-	".MTS":     ".MTS",
-	".CDR":     ".CDR",
-	".BMP":     ".BMP",
-	".NEF":     ".NEF",
-	".ZIP":     ".ZIP",
-	".3GP":     ".3GP",
-	".CR2":     ".CR2",
-	".PSD":     ".PSD",
+	".JPG":  ".JPG",
+	".JPEG": ".JPEG",
+	/* 	".PNG":     ".PNG",
+	   	".GIF":     ".GIF",
+	   	".RAW":     ".RAW",
+	   	".RW2":     ".RW2",
+	   	".TIF":     ".TIF",
+	   	".AI":      ".AI",
+	   	".MOV":     ".MOV",
+	   	".RAR":     ".RAR",
+	   	".MPG":     ".MPG",
+	   	".AFPHOTO": ".AFPHOTO",
+	   	".AVI":     ".AVI",
+	   	".M4V":     ".M4V",
+	   	".WMV":     ".WMV",
+	   	".MP4":     ".MP4",
+	   	".MTS":     ".MTS",
+	   	".CDR":     ".CDR",
+	   	".BMP":     ".BMP",
+	   	".NEF":     ".NEF",
+	   	".ZIP":     ".ZIP",
+	   	".3GP":     ".3GP",
+	   	".CR2":     ".CR2",
+	   	".PSD":     ".PSD", */
 }
 
 var ignoreExtensionMap = map[string]int{}
 
 var crc32q = crc32.MakeTable(0xD5828281)
 
+var crc32FileMap = map[uint32][]string{}
+
 func main() {
 	c := make(chan chanDirInfo)
-	go mapDirectories("d:/Images", c)
-	dirinfo := <-c
-	fmt.Printf("Main directory contains %d\n\n", dirinfo.cnt)
+	go mapDirectories("c:/Users/jarom/Pictures", c)
+	<-c
 
-	/* 	for _, v := range dirinfo.dirs {
-		fmt.Printf("%-60s %-40s %d\n", v.path, v.name, v.files)
-	} */
-
-	for k, v := range ignoreExtensionMap {
-		fmt.Printf("%-40s %-10d\n", k, v)
+	for _, v := range crc32FileMap {
+		if len(v) > 1 {
+			fmt.Println(v)
+		}
 	}
+
+	/* 	for k, v := range ignoreExtensionMap {
+		fmt.Printf("%-40s %-10d\n", k, v)
+	} */
 }
 
 func mapDirectories(path string, c chan chanDirInfo) {
@@ -82,22 +85,24 @@ func mapDirectories(path string, c chan chanDirInfo) {
 			c := make(chan chanDirInfo)
 			go mapDirectories(f, c)
 			dirinfo := <-c
+			fmt.Printf("\r checked files in %s\t\t\t\t\t", f)
 			dirmap = append(dirmap, dirInfo{file.Name(), path, dirinfo.cnt})
 			dirmap = append(dirmap, dirinfo.dirs...)
 		} else {
-			filename := strings.ToUpper(filepath.Ext(file.Name()))
-			if _, ok := extesionMap[filename]; ok {
+			fileext := strings.ToUpper(filepath.Ext(file.Name()))
+			if _, ok := extesionMap[fileext]; ok {
 				filecount++
-				fcontent, err := ioutil.ReadFile(f)
+				body, err := ioutil.ReadFile(f)
 
 				if err != nil {
 					fmt.Println("Error reading file", err)
 					continue
 				}
-				fmt.Printf("%08x\n", crc32.Checksum(fcontent, crc32q))
+				checksum := crc32.Checksum(body, crc32q)
+				crc32FileMap[checksum] = append(crc32FileMap[checksum], f)
 			} else {
 				//collect all not checked file extension
-				ignoreExtensionMap[filename]++
+				ignoreExtensionMap[fileext]++
 			}
 		}
 	}
